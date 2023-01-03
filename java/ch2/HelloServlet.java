c28d68dbf0b7d05bd992b5726f321a106dd140c1
fbgkdn2484 <80086225+fbgkdn2484@users.noreply.github.com>
2023-01-03 16:49:49 +0900

    Add files via upload

---
+++ java/ch2/HelloServlet.java
@@ -0,0 +1,58 @@
+package ch2;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html; charset=UTF-8");
+		try {
+			PrintWriter pw = response.getWriter();
+			pw.println("<HTML>");
+			pw.println("<head><title>Servlet 연습</title></head>");
+			pw.println("<body>");
+			pw.println("<h2>Servlet 입문</h2>");
+			pw.println("<hr>");
+			pw.println("처음으로 작성하는 Servlet");
+			pw.println("</body>");
+			pw.println("</HTML>");
+			pw.close();
+		}
+		catch(Exception e) {
+			getServletContext().log("Error in HelloServlet:" , e);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ webapp/ch6/IfElseTestForm2.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>로그인 폼</title>
+</head>
+<body>
+	<h3>아이디와 패스워드를 입력하세요.</h3>
+	<hr>
+	<form method="post" action="IfElseTest2Pro.jsp">
+		아이디 : <input	 type = "text" name = "id"><br>
+		패스워드 : <input	 type = "password" name = "pwd"><br><br>
+		<input	 type = "submit" value = "입력완료"><br>
+	</form>
+</body>
+</html>
